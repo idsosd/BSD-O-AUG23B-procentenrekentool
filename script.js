faf618c75980054b7e0a482d0f9c62769c6f0e7b
ip.osinga <ip.osinga@alfa-college.nl>
2024-03-14 09:22:42 +0100

    v2024-03-14

---
+++ script.js
@@ -0,0 +1,9 @@
+function solveProblem()
+{
+    alert("Hij doet het en Jason ziet er uit als een toffe banaan")
+}
+
+function checkInformation()
+{
+    alert("Jason doet niet alsof hij oplet en Robin al helemaal niet; hij zegt van wel;")
+}
